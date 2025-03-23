411c485582b81d24dd2b97413ba943f3ef3a9aaa
Hiệpvk <51714777+hiepvk@users.noreply.github.com>
2025-03-23 18:06:41 +0700

    Rename coccoc.ps1 to index.ps1

--- coccoc/coccoc.ps1
+++
@@ -1,1 +0,0 @@
-Invoke-WebRequest -Uri https://files1.coccoc.com/browser/x64/coccoc_vi_machine.exe -OutFile .\coccoc_vi_machine.exe
---
+++ coccoc/index.ps1
@@ -0,0 +1,1 @@
+Invoke-WebRequest -Uri https://files1.coccoc.com/browser/x64/coccoc_vi_machine.exe -OutFile .\coccoc_vi_machine.exe
