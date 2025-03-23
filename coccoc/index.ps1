21bb1d02c3f2c94f05980d33a6f1227aefcc016c
Hiệpvk <51714777+hiepvk@users.noreply.github.com>
2025-03-23 18:27:28 +0700

    Delete coccoc/index.ps1

--- coccoc/index.ps1
+++
@@ -1,3 +0,0 @@
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]'Tls,Tls11,Tls12'
-Invoke-WebRequest -Uri https://files1.coccoc.com/browser/x64/coccoc_vi_machine.exe -OutFile .\coccoc_vi_machine.exe
-.\coccoc_vi_machine.exe
