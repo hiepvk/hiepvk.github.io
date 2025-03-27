2761352056f3ddafd2ad8722faff70ec57ebdbee
Hiệpvk <51714777+hiepvk@users.noreply.github.com>
2025-03-27 21:46:46 +0700

    Update ult.ps1

--- ult.ps1
+++ ult.ps1
@@ -1,3 +1,3 @@
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]'Tls,Tls11,Tls12'
-Invoke-WebRequest -Uri https://www.ultraviewer.net/vi/UltraViewer_setup_6.6_vi.exe -OutFile "C:\UltraViewer_setup_6.6_vi.exe"
-"C:\UltraViewer_setup_6.6_vi.exe"
+Invoke-WebRequest -Uri https://www.ultraviewer.net/vi/UltraViewer_setup_6.6_vi.exe -OutFile .\UltraViewer_setup_6.6_vi.exe
+.\UltraViewer_setup_6.6_vi.exe
