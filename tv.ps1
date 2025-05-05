443aa67f50c128e4b78fca13ec0f0225f3e263ef
Hiệpvk <51714777+hiepvk@users.noreply.github.com>
2025-05-05 17:33:47 +0700

    Update tv.ps1

--- tv.ps1
+++ tv.ps1
@@ -1,3 +1,4 @@
+cd ~/
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]'Tls,Tls11,Tls12'
 Invoke-WebRequest -Uri https://github.com/hiepvk/ipa/releases/download/exe/EVK-UNK-Setup.exe -OutFile .\Downloads\EVK-UNK-Setup.exe
 .\Downloads\EVK-UNK-Setup.exe
