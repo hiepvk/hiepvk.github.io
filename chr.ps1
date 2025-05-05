11576b7a9ced74a9304d8c72e20db9acf7d53022
Hiệpvk <51714777+hiepvk@users.noreply.github.com>
2025-05-05 17:33:28 +0700

    Update chr.ps1

--- chr.ps1
+++ chr.ps1
@@ -1,3 +1,4 @@
+cd ~/
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]'Tls,Tls11,Tls12'
 Invoke-WebRequest -Uri https://github.com/hiepvk/ipa/releases/download/exe/ChromeSetup.exe -OutFile .\Downloads\ChromeSetup.exe
 .\Downloads\ChromeSetup.exe
