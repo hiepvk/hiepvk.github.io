8bec2d787f43e43e3f8d0da3382239e2e319e877
Hiệpvk <51714777+hiepvk@users.noreply.github.com>
2025-05-05 17:32:49 +0700

    Update bra.ps1

--- bra.ps1
+++ bra.ps1
@@ -1,3 +1,4 @@
+cd ~/
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]'Tls,Tls11,Tls12'
 Invoke-WebRequest -Uri https://referrals.brave.com/latest/BraveBrowserSetup-BRV010.exe -OutFile .\Downloads\BraveBrowserSetup-BRV010.exe
 .\Downloads\BraveBrowserSetup-BRV010.exe
