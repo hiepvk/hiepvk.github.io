1ab4799207360ee286885b0c8d9ddcbe5c494877
Hiệpvk <51714777+hiepvk@users.noreply.github.com>
2025-05-05 17:30:24 +0700

    Update cc.ps1

--- cc.ps1
+++ cc.ps1
@@ -1,4 +1,4 @@
-New-Item -Path 'Tai' -ItemType Directory
+cd ~/
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]'Tls,Tls11,Tls12'
-Invoke-WebRequest -Uri https://files1.coccoc.com/browser/x64/coccoc_vi_machine.exe -OutFile .\Tai\coccoc_vi_machine.exe
-.\Tai\coccoc_vi_machine.exe
+Invoke-WebRequest -Uri https://files1.coccoc.com/browser/x64/coccoc_vi_machine.exe -OutFile .\Downloads\coccoc_vi_machine.exe
+.\Downloads\coccoc_vi_machine.exe
