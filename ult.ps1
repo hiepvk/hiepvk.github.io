1f34adea323f2a1361d613a2787e0b9581369a09
Hiệpvk <51714777+hiepvk@users.noreply.github.com>
2025-09-17 12:11:51 +0700

    Update ult.ps1

--- ult.ps1
+++ ult.ps1
@@ -2,46 +2,28 @@
 
 # Chạy với quyền quản trị viên (Run as Administrator)
 
-
-
 $Path = $ENV:TEMP
 
 $Installer = "UltraViewer_setup_6.6_vi.exe"
 
-
-
-Write-Host "Đang tải xuống UltraViewer..."
-
-
+Write-Host "Dang tai xuong UltraViewer..."
 
 # Tải file cài đặt
 
 Invoke-WebRequest "https://www.ultraviewer.net/vi/UltraViewer_setup_6.6_vi.exe" -OutFile "$Path\$Installer"
 
-
-
-Write-Host "Đã tải xuống thành công."
-
-Write-Host "Đang bắt đầu quá trình cài đặt..."
-
-
+Write-Host "Da tai xuong thanh cong."
+Write-Host "Dang bat dau qua trinh cai dat..."
 
 # Chạy trình cài đặt ở chế độ im lặng với quyền quản trị viên
 
 Start-Process -FilePath "$Path\$Installer" -Args "/silent /install" -Verb RunAs -Wait
 
-
-
-Write-Host "Cài đặt hoàn tất."
-
-Write-Host "Đang dọn dẹp file cài đặt..."
-
-
+Write-Host "Cai dat hoan tat."
+Write-Host "Dang don dep file cai dat..."
 
 # Xóa file cài đặt đã tải xuống
 
 Remove-Item "$Path\$Installer" -ErrorAction SilentlyContinue
 
-
-
-Write-Host "Hoàn tất."
+Write-Host "Hoan tat."
