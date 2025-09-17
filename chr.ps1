417ed7b480c6c558d560688a82f4ae092caaa28c
Hiệpvk <51714777+hiepvk@users.noreply.github.com>
2025-09-17 12:13:08 +0700

    Update chr.ps1

--- chr.ps1
+++ chr.ps1
@@ -2,46 +2,24 @@
 
 # Chạy với quyền quản trị viên (Run as Administrator)
 
-
-
 $Path = $ENV:TEMP
-
 $Installer = "ChromeSetup.exe"
 
-
-
-Write-Host "Đang tải xuống Chrome..."
-
-
+Write-Host "Dang tai xuong Chrome..."
 
 # Tải file cài đặt
-
 Invoke-WebRequest "https://github.com/hiepvk/ipa/releases/download/exe/ChromeSetup.exe" -OutFile "$Path\$Installer"
 
+Write-Host "Da tai xuong thanh cong."
+Write-Host "Dang bat dau qua trinh cai dat..."
 
-
-Write-Host "Đã tải xuống thành công."
-
-Write-Host "Đang bắt đầu quá trình cài đặt..."
-
-
-
-# Chạy trình cài đặt ở chế độ im lặng với quyền quản trị viên
-
+# Run the installer in silent mode with administrator privileges
 Start-Process -FilePath "$Path\$Installer" -Args "/silent /install" -Verb RunAs -Wait
 
+Write-Host "Cai dat hoan tat."
+Write-Host "Dang don dep file cai dat..."
 
-
-Write-Host "Cài đặt hoàn tất."
-
-Write-Host "Đang dọn dẹp file cài đặt..."
-
-
-
-# Xóa file cài đặt đã tải xuống
-
+# Delete the downloaded installer file
 Remove-Item "$Path\$Installer" -ErrorAction SilentlyContinue
 
-
-
-Write-Host "Hoàn tất."
+Write-Host "Hoan tat."
