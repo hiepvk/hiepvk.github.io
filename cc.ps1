7f2f24171590ef7646b2eac5dc9cc7b32a28c840
Hiệpvk <51714777+hiepvk@users.noreply.github.com>
2025-09-17 12:14:30 +0700

    Update cc.ps1

--- cc.ps1
+++ cc.ps1
@@ -1,47 +1,24 @@
 # Script để tải xuống và cài đặt CocCoc
-
 # Chạy với quyền quản trị viên (Run as Administrator)
 
-
-
 $Path = $ENV:TEMP
-
 $Installer = "coccoc_vi_machine.exe"
 
-
-
-Write-Host "Đang tải xuống CocCoc..."
-
-
+Write-Host "Dang tai xuong CocCoc..."
 
 # Tải file cài đặt
-
 Invoke-WebRequest "https://files1.coccoc.com/browser/x64/coccoc_vi_machine.exe" -OutFile "$Path\$Installer"
 
+Write-Host "Da tai xuong thanh cong."
+Write-Host "Dang bat dau qua trinh cai dat..."
 
-
-Write-Host "Đã tải xuống thành công."
-
-Write-Host "Đang bắt đầu quá trình cài đặt..."
-
-
-
-# Chạy trình cài đặt ở chế độ im lặng với quyền quản trị viên
-
+# Run the installer in silent mode with administrator privileges
 Start-Process -FilePath "$Path\$Installer" -Args "/silent /install" -Verb RunAs -Wait
 
+Write-Host "Cai dat hoan tat."
+Write-Host "Dang don dep file cai dat..."
 
-
-Write-Host "Cài đặt hoàn tất."
-
-Write-Host "Đang dọn dẹp file cài đặt..."
-
-
-
-# Xóa file cài đặt đã tải xuống
-
+# Delete the downloaded installer file
 Remove-Item "$Path\$Installer" -ErrorAction SilentlyContinue
 
-
-
-Write-Host "Hoàn tất."
+Write-Host "Hoan tat."
