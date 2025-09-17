f87939250ce22ffd307c50a4d180a8fbbd96be4a
Hiệpvk <51714777+hiepvk@users.noreply.github.com>
2025-09-17 12:13:47 +0700

    Update bra.ps1

--- bra.ps1
+++ bra.ps1
@@ -1,47 +1,24 @@
 # Script để tải xuống và cài đặt Brave
-
 # Chạy với quyền quản trị viên (Run as Administrator)
 
-
-
 $Path = $ENV:TEMP
-
 $Installer = "BraveBrowserSetup-BRV010.exe"
 
-
-
-Write-Host "Đang tải xuống Brave..."
-
-
+Write-Host "Dang tai xuong Brave..."
 
 # Tải file cài đặt
-
 Invoke-WebRequest "https://referrals.brave.com/latest/BraveBrowserSetup-BRV010.exe" -OutFile "$Path\$Installer"
 
+Write-Host "Da tai xuong thanh cong."
+Write-Host "Dang bat dau qua trinh cai dat..."
 
-
-Write-Host "Đã tải xuống thành công."
-
-Write-Host "Đang bắt đầu quá trình cài đặt..."
-
-
-
-# Chạy trình cài đặt ở chế độ im lặng với quyền quản trị viên
-
+# Run the installer in silent mode with administrator privileges
 Start-Process -FilePath "$Path\$Installer" -Args "/silent /install" -Verb RunAs -Wait
 
+Write-Host "Cai dat hoan tat."
+Write-Host "Dang don dep file cai dat..."
 
-
-Write-Host "Cài đặt hoàn tất."
-
-Write-Host "Đang dọn dẹp file cài đặt..."
-
-
-
-# Xóa file cài đặt đã tải xuống
-
+# Delete the downloaded installer file
 Remove-Item "$Path\$Installer" -ErrorAction SilentlyContinue
 
-
-
-Write-Host "Hoàn tất."
+Write-Host "Hoan tat."
